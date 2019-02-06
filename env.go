66c72d4b61e2cb1fc190ba1638f9f27ef6c042f0
Dan <gitkraken@aerolith.net>
2019-02-05 21:08:27 -0500

    honor XDG env

--- env.go
+++ env.go
@@ -11,12 +11,20 @@ func getCachePath(flag string) string {
 	if flag != "" {
 		return flag
 	}
+	cache := os.Getenv("XDG_CACHE_HOME")
+	if cache != "" {
+		return fmt.Sprintf("%v/wallhaven/", cache)
+	}
 	return fmt.Sprintf("%v/.cache/wallhaven/", home)
 }
 
 func getConfigPath(flag string) string {
 	if flag != "" {
 		return flag
+	}
+	config := os.Getenv("XDG_CONFIG_HOME")
+	if config != "" {
+		return fmt.Sprintf("%v/wallhaven/", config)
 	}
 	return fmt.Sprintf("%v/.config/wallhaven/", home)
 }
